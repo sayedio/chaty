ca2b54b324e9a9c4247920c93e8c09d06ad93375
SayedSheikh <sayedsheikh9@gmail.com>
2026-05-18 01:29:20 +0600

    feat: enhance error handling and add new views for inbox, login, and user management

--- middlewares/common/errorHandlers.js
+++ middlewares/common/errorHandlers.js
@@ -4,7 +4,19 @@ function notFoundError(req, res, next) {
   next(createError(404, "Your requested content was not found"));
 }
 function handleError(err, req, res, next) {
-  res.render("error", { title: "Error page" });
+  res.locals.error = process.env.NODE_ENV === "development" ? err : err.message;
+  if (!res.locals.html) {
+    res.render("error", {
+      title: "Error page",
+      desc: res.locals.error,
+    });
+  } else {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+      error: res.locals.error,
+    });
+  }
 }
 
 module.exports = {
